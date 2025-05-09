1213f28735946592820417cf9ef8a075b95d412d
QuangAnh28 <nguyenquocquanganh28@gmail.com>
2025-05-09 09:01:02 +0700

    Update Game.java

--- game_managment/src/Game.java
+++ game_managment/src/Game.java
@@ -1,4 +1,35 @@
 public class Game {
+    private int gameId;
+    private String title;
+    private String genre;
+    private String platform;
+    private String releaseDate;
+    private String description;
+    private String coverImage;
+
+    public Game(int gameId, String title, String genre, String platform, String releaseDate, String description, String coverImage) {
+        this.gameId = gameId;
+        this.title = title;
+        this.genre = genre;
+        this.platform = platform;
+        this.releaseDate = releaseDate;
+        this.description = description;
+        this.coverImage = coverImage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    @Override
+    public String toString() {
+        return title + " (" + platform + ") - " + genre;
+    }
+}
 
     
 }
