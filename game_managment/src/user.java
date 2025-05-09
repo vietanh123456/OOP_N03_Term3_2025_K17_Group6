3a61cbf171abfb193df5a7e926b3a52560437ebe
QuangAnh28 <nguyenquocquanganh28@gmail.com>
2025-05-09 09:21:36 +0700

    Update user.java

---
+++ game_managment/src/user.java
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, user_id, username, email, avatar=None):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.avatar = avatar
+        self.library = []  # danh sách các game của người dùng
+
+    def add_game(self, user_game):
+        self.library.append(user_game)
+
+    def __str__(self):
+        return f"User: {self.username} ({self.email})"
