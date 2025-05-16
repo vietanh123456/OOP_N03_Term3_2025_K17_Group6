72f30f4d16e168fdbffd8c3615720149669f0ff1
nguyenthecuong05 <tuilaaimaailatui3@gmail.com>
2025-05-16 01:18:05 +0000

    User

---
+++ game_managment/src/User.java
@@ -0,0 +1,22 @@
+public class User {
+    private String username;
+    private String email ;
+    private String password ;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getUsername(){
+        return username;
+
+    }
+    public void abc(){
+}
+}
---
+++ game_managment/test/TestUser.java
@@ -0,0 +1,8 @@
+class TestUser{
+
+    public static void test(){
+        User u = new User("Nguyen", "nguyen@gmail.com", "*****");
+        System.out.println(u.getUsername());
+
+    }
+}
