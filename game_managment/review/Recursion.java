a0771da850be1685900c51d5377b75f5f195b455
nguyenthecuong05 <tuilaaimaailatui3@gmail.com>
2025-05-16 01:42:26 +0000

    Time

--- game_managment/review/Recursion.java
+++ game_managment/review/Recursion.java
@@ -1,4 +1,17 @@
-class Recursion{
+public class Recursion {
 
-    
+
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
