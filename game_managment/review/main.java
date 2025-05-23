39d6d7f8b0f45f0cafb00ad2b97c034fbd9689d3
QuangAnh28 <nguyenquocquanganh28@gmail.com>
2025-05-23 09:23:20 +0700

    Update main.java

---
+++ game_managment/review/main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        GameList gameList = new GameList();
+
+        // Thêm game
+        gameList.addGame(new Game("Game A", "PC", "Hành động", "Đang chơi"));
+        gameList.addGame(new Game("Game B", "PS5", "Phiêu lưu", "Chưa chơi"));
+
+        // In danh sách
+        gameList.printGameList();
+
+        // Xóa game
+        gameList.deleteGame("Game B", "PS5");
+
+        // In lại danh sách sau xóa
+        gameList.printGameList();
+    }
+}
