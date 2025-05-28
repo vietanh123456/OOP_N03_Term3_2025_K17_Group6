a1ded261446d341e28656d32c52e5402fb5510bf
QuangAnh28 <nguyenquocquanganh28@gmail.com>
2025-05-28 23:58:44 +0700

    Update Game.java

--- game_managment/src/Game.java
+++ game_managment/src/Game.java
@@ -1,28 +1,81 @@
-public class Game {
-    private int id;
-    private String name;
-    private String genre;
-    private String platform;
+import java.time.LocalDate;
 
-    public Game(int id, String name, String genre, String platform) {
-        this.id = id;
-        this.name = name;
+public class Game {
+    private int game_id;
+    private String title;
+    private String genre;
+    private LocalDate release_date;
+    private String description;
+    private String cover_image;
+
+    // Constructor
+    public Game(int game_id, String title, String genre, LocalDate release_date, String description, String cover_image) {
+        this.game_id = game_id;
+        this.title = title;
         this.genre = genre;
-        this.platform = platform;
+        this.release_date = release_date;
+        this.description = description;
+        this.cover_image = cover_image;
     }
 
-    // Getters & Setters
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public String getGenre() { return genre; }
-    public String getPlatform() { return platform; }
+    // Getter và Setter
+    public int getGameId() {
+        return game_id;
+    }
 
-    public void setName(String name) { this.name = name; }
-    public void setGenre(String genre) { this.genre = genre; }
-    public void setPlatform(String platform) { this.platform = platform; }
+    public void setGameId(int game_id) {
+        this.game_id = game_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return release_date;
+    }
+
+    public void setReleaseDate(LocalDate release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoverImage() {
+        return cover_image;
+    }
+
+    public void setCoverImage(String cover_image) {
+        this.cover_image = cover_image;
+    }
 
     @Override
     public String toString() {
-        return "[" + id + "] " + name + " - " + genre + " - " + platform;
+        return "Game{" +
+                "game_id=" + game_id +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", release_date=" + release_date +
+                ", description='" + description + '\'' +
+                ", cover_image='" + cover_image + '\'' +
+                '}';
     }
 }
