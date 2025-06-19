9093b0f110244f6d1494030a0045f896114e6c76
nguyenthecuong05 <tuilaaimaailatui3@gmail.com>
2025-06-19 01:11:42 +0000

    trycatch

--- game_managment/src/NameInput.java
+++ game_managment/src/NameInput.java
@@ -3,19 +3,30 @@ import java.util.Scanner;
 public class NameInput {
 
     public static void nameEnter(User user, Scanner sc) {
-        System.out.print("Nhập tên game cần tìm: ");
-        String tenTim = sc.nextLine().toLowerCase();
-        boolean timThay = false;
 
-        for (UserGame ug : user.getDanhSachGame()) {
-            if (ug.getGame().getTenGame().toLowerCase().contains(tenTim)) {
-                System.out.println(ug);
-                timThay = true;
+        try {
+            System.out.print("Nhập tên game cần tìm: ");
+            String tenTim = sc.nextLine().toLowerCase();
+            boolean timThay = false;
+
+            for (UserGame ug : user.getDanhSachGame()) {
+                if (ug.getGame().getTenGame().toLowerCase().contains(tenTim)) {
+                    System.out.println(ug);
+                    timThay = true;
+                }
             }
+
+            if (!timThay) {
+                System.out.println("Không tìm thấy game.");
+            }
+
         }
 
-        if (!timThay) {
-            System.out.println("Không tìm thấy game.");
+        catch (Exception e) {
+
+            System.out.println(e);
+
         }
+
     }
 }
